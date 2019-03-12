dfb5a7ca7c67e5d1b10fd66cc8372c6e221e37b5
Tanay Vardhan <tvardha2@illinois.edu>
2019-03-12 11:55:07 -0500

    Updates

--- component---src-pages-page-2-js-e768c8d883ff5bcd34ad.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{138:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(142),l=t(143);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-e768c8d883ff5bcd34ad.js.map
